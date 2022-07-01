37ecdd2e9e6c741e5ec64be73f3c31ac949c37fe
azinjmlifr <azinjamalifar@gmail.com>
2022-07-01 14:41:32 +0430

    add cw14

---
+++ CW/cw14Friday.js
@@ -0,0 +1,7 @@
+const API_URL = "https://62ac07b69fa81d00a7a869f3.mockapi.io/todoList";
+
+$(document).ready(function () {
+  $.ajax({ url: API_URL }).done(function (toDoList) {
+    $.each();
+  });
+});
